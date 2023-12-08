958f07898c302addcc78d2964984587312dcbe7c
kortekoski <mika.kortekoski@gmail.com>
2023-12-08 15:22:31 +0200

    router module added

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -2,35 +2,11 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
+app.use('/api/blogs', blogsRouter)
 
 module.exports = app
